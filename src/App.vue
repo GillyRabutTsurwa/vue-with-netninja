6994049c23d9e1e75e2536c583dd2210b36e7c6a
GillyRabutTsurwa <tsurwagilbert@gmail.com>
2020-05-02 17:20:37 -0500

    part 7 slots

--- src/App.vue
+++ src/App.vue
@@ -1,43 +1,31 @@
 <template>
   <div id="app">
     <img class="logo" alt="Vue logo" src="./assets/logo.png" />
-    <!-- IMPORTANTNOTE: Here in the parent, we listen to the emitted event like any other click event, with v-on:{eventName} or :{eventName}. Onwards, we usually declare a function with an arguement of ($event), which is the data that we wanted to bring with from the child component. In our case, it was Vue Makengos. -->
-    <HeaderCompo v-bind:titleProp="title" v-on:title-changed="updateTitle($event)" />
-    <NinjaCompo v-bind:ninjasProp="ninjas" />
-
-    <FooterCompo v-bind:titleProp="title" />
+    <SlotDemo>
+      <h2 slot="titleSlot">{{slotTitle}}</h2>
+      <p slot="textSlot">Pinchi ma pinchi</p>
+    </SlotDemo>
+    <hr />
+    <FormHelper />
   </div>
 </template>
 
 <script>
-import Header from "./components/Header";
-import Ninjas from "./components/Ninjas";
-import Footer from "./components/Footer";
+import SlotDemo from "./components/SlotDemo";
+import FormHelper from "./components/FormHelper";
 export default {
   name: "App",
-  components: {
-    HeaderCompo: Header,
-    NinjaCompo: Ninjas,
-    FooterCompo: Footer
-  },
   data() {
     return {
-      ninjas: [
-        { name: "Ryu", speciality: "Vue Components", show: false },
-        { name: "Crystal", speciality: "HTML Engineering", show: false },
-        { name: "Hitoshi", speciality: "Click Events", show: false },
-        { name: "Tango", speciality: "Conditionals", show: false },
-        { name: "Kami", speciality: "Webpack", show: false },
-        { name: "Yoshi", speciality: "Data Diggin", show: false }
-      ],
-      title: "Vue Ninjas"
+      //IMPORTANTNOTE: If we want to make our slots dynamic, we must use them where the dynamic slots are initialised (in our case the app component).
+      //HINT: where you are NOT using the slot tags to define them like this....
+      // <slot></slot>
+      slotTitle: "Je suis un titre de slot dynamique"
     };
   },
-  methods: {
-    //IMPORTANTNOTE: This is the method that has access to the data passed through from the child component for the event. The parametre updatedTitle IS $event arguement that contains the data to pass on to the parent... Vue Makengos
-    updateTitle(updatedTitle) {
-      this.title = updatedTitle;
-    }
+  components: {
+    SlotDemo: SlotDemo,
+    FormHelper: FormHelper
   }
 };
 </script>
---
+++ src/components/SlotDemo.vue
@@ -0,0 +1,19 @@
+<template>
+  <!-- NEW: SLOTS. This will be pertaining to the html inside our component tags on the App component -->
+  <!-- NEWNOTE: naming slots is even better, parce que tu peux deplacer les slots dans les endroits differences -->
+  <div>
+    <slot name="titleSlot"></slot>
+    <h1>Yo</h1>
+    <slot name="textSlot"></slot>
+  </div>
+</template>
+
+<script>
+export default {};
+</script>
+
+<style>
+h1 {
+  color: forestgreen;
+}
+</style>
