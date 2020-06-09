50e7a125deb6da663f60d51c1d2e11d0fa3ed358
GillyRabutTsurwa <tsurwagilbert@gmail.com>
2020-06-09 04:58:06 -0500

    input binding... i think

--- src/components/Header.vue
+++ src/components/Header.vue
@@ -1,6 +1,5 @@
 <template>
   <header>
-    <!-- NOTEIMPORTANT: Since a titleProp (a String) is a reference type, it will not change the value anywhere else, because we are making a duplicate of it and not using the same reference point as we do in reference types. -->
     <h1 v-on:click="changeTitle">{{titleProp}}</h1>
   </header>
 </template>
