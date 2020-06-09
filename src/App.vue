890bd066235ba2cb65fbabb17539ebcd6febd215
GillyRabutTsurwa <tsurwagilbert@gmail.com>
2020-06-08 19:55:00 -0500

    input binding

--- src/App.vue
+++ src/App.vue
@@ -1,53 +1,32 @@
 <template>
   <div id="app">
     <img class="logo" alt="Vue logo" src="./assets/logo.png" />
-    <button
-      @click="changeForm"
-    >{{component === "FormOne" ? "Change To Form 2" : "Change To Form 1"}}</button>
-    <!-- NEW component tag -->
-    <!-- component vue "element" has the is attribute which looks for a value that equates to the desired form name to be rendered. The value have to equate to the component name(s) -->
-    <!-- NEW keep alive tag. keeps the component alive and hidden (does not destroy it) when we dynamically change to another component. This is especially useful cause we can switch between the forms and the info in the inputs will be preserved -->
-    <!-- NOTE: Shaun's definition keep alive tells component: Do not destroy the component (its data etc) when we switch to a new one -->
-    <!-- NOTE: definition of keep alive according to VSCode: When wrapped around a dynamic component, <keep-alive> caches the inactive component instances without destroying them. -->
-    <keep-alive>
-      <component v-bind:is="component"></component>
-    </keep-alive>
+    <AddBlog />
   </div>
 </template>
 
 <script>
-import FormOne from "./components/FormOne";
-import FormTwo from "./components/FormTwo";
+import AddBlog from "./components/AddBlog";
 export default {
   name: "App",
   data() {
-    return {
-      // notice that the value of the component variable is the exact name of the Component. This also applies of the component you want to switch to.
-      component: "FormOne"
-    };
+    return {};
   },
   components: {
-    FormOne: FormOne,
-    FormTwo: FormTwo
+    AddBlog: AddBlog
   },
-  methods: {
-    changeForm() {
-      this.component === "FormOne"
-        ? (this.component = "FormTwo")
-        : (this.component = "FormOne");
-    }
-  }
+  methods: {}
 };
 </script>
 
 <style>
+@import "./App.css";
 #app {
   font-family: Avenir, Helvetica, Arial, sans-serif;
   -webkit-font-smoothing: antialiased;
   -moz-osx-font-smoothing: grayscale;
   text-align: center;
   color: #2c3e50;
-  margin-top: 60px;
 }
 
 .logo {
---
+++ src/components/AddBlog.vue
@@ -0,0 +1,78 @@
+<template>
+  <div id="add-blog">
+    <h3>Add Blog</h3>
+    <form>
+      <label for="title">Blog Title:</label>
+      <input type="text" id="title" v-model.lazy="blog.title" />
+      <label for="content">Blog Content:</label>
+      <textarea name id="content" v-model.lazy="blog.content"></textarea>
+    </form>
+    <div class="preview">
+      <div class="preview__title">
+        <p>
+          <strong>Blog Title:</strong>
+          {{blog.title}}
+        </p>
+      </div>
+      <div class="preview__content">
+        <p>
+          <strong>Blog Content:</strong>
+        </p>
+        <p>{{blog.content}}</p>
+      </div>
+    </div>
+  </div>
+</template>
+
+<script>
+export default {
+  data() {
+    return {
+      blog: {
+        title: "",
+        content: ""
+      }
+    };
+  }
+};
+</script>
+
+<style>
+#add-blog {
+  margin: 20px auto;
+  max-width: 500px;
+}
+
+form,
+.preview {
+  display: flex;
+  flex-direction: column;
+  align-items: flex-start;
+}
+
+.preview {
+  padding: 10px 20px;
+  border: 1px dotted #ccc;
+  margin: 30px 0;
+}
+
+.preview__content {
+  display: flex;
+  flex-direction: column;
+  align-items: flex-start;
+}
+label {
+  font-weight: bold;
+  margin: 0.5em 0;
+}
+input[type="text"],
+textarea {
+  display: block;
+  width: 100%;
+  padding: 8px;
+}
+
+h3 {
+  margin-top: 10px;
+}
+</style>
