5c07e803a09f5bf8c02497bdc8f173efcff47978
GillyRabutTsurwa <tsurwagilbert@gmail.com>
2020-06-15 10:45:08 -0500

    hash vs history. Again, Shaun explains a complex concept soooo well, that I could understand this with half my IQ diminished

--- src/main.js
+++ src/main.js
@@ -8,8 +8,11 @@ Vue.use(VueRouter);
 // code to instantiate vue router
 const router = new VueRouter({
   // object for our routes. we will not do it here, we will make a routes folder & file
-  routes: Routes
+  routes: Routes,
+  mode: "history"
+  // IMPORTANTNOTE: mode: "history" will work locally. If you want to make it work if you are deploying an application (preferrably using nodejs), consult this link: https://router.vuejs.org/guide/essentials/history-mode.html#example-server-configurations 
 })
+
 
 Vue.config.productionTip = false;
 
